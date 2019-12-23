a248945e520e18f071543889273799c70c2e3f0e
Anna <annalanzino@ufl.edu>
2019-12-23 11:26:40 -0500

    trying to add counter

--- src/Bot/Bot.js
+++ src/Bot/Bot.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { botData } from './botData'
 import Task from './Task'
 import './Bot.css'
 
 const Bot = ({ name, type }) => {
+  const [completeTasks, setCompleteTasks] = useState(0)
+
+  const incrementCompletedTasks = () => {
+    setCompleteTasks(completeTasks+1)
+  }
+
   return (
     <div className="Bot">
       <header>
         <h2 className="botHeader">{name}</h2>
         <h3 className="botHeader">{type}</h3>
+        <h4 className="botHeader">Completed Tasks: {completeTasks}</h4>
       </header>
       {
-        botData[type].tasks.map(t => <Task task={t.task} eta={t.eta} key={t.eta}/>)
+        botData[type].tasks.map(t => <Task task={t.task} eta={t.eta} key={t.eta} onTaskCompletion={incrementCompletedTasks}/>)
       }
     </div>
   );
--- src/Bot/Task.js
+++ src/Bot/Task.js
@@ -4,13 +4,13 @@ import loading from '../images/loading.gif'
 import checkmark from '../images/checkmark.png'
 import './Bot.css'
 
-const Task = ({ task, eta }) => {
-  console.log(task)
+const Task = ({ task, eta, onTaskCompletion }) => {
   const [isLoading, setLoading] = useState(false)
   const [isCompleted, setCompleted] = useState(false)
   const seconds = eta / 1000;
 
   const taskCompletion = () => {
+    onTaskCompletion()
     setLoading(false)
     setCompleted(true)
   }
@@ -22,18 +22,17 @@ const Task = ({ task, eta }) => {
 
   return (
     <div>
-      <p className="task">{task} {seconds} {seconds > 1 ? "seconds": "second"}</p>
+      {
+        isCompleted ? <p className="completedTask">{task} {seconds} {seconds > 1 ? "seconds": "second"}</p> : 
+        <p className="task">{task} {seconds} {seconds > 1 ? "seconds": "second"}</p>
+      }
       {
         isCompleted ? <img src={checkmark} className="image" alt="checkmark"/> :
         (isLoading ? <img src={loading} className="image" alt="loading" /> : 
-        <button onClick={handleTaskCompletion} type="button">Complete Task</button>)
+        <button onClick={handleTaskCompletion} type="button" className="taskButton">complete</button>)
       }
     </div>
   );
 }
-
-{/* <header className="App-header">
-<img src={robot} className="App-logo" alt="logo" />
-</header> */}
 
 export default Task
